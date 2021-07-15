fec78df8906447c72bcd741d52222530f5905753
rtamburro <ronnie.tamburro@protonmail.com>
2021-07-14 22:40:01 -0400

    link to TOP

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export default function Home() {
                 <h1 className="mb-8 text-2xl font-bold leading-none text-white lg:leading-snug">Hi, my name is</h1>
                 <h2 className="mb-8 text-6xl font-bold text-neonPink">Ronnie</h2>
                 <h1 className="mb-8 text-6xl font-bold leading-none text-white lg:leading-snug">A Self-Taught Web Developer</h1>
-                <p className="w-1/2 text-xl text-white">I'm currently working through <span className="font-bold underline text-odin">The Odin Project</span> as well as a personal project when time allows. A recent accomplishment of mine is condensing my first-ever Python script from 600+ lines of code down to 24 lines!</p>
+                <p className="w-1/2 text-xl text-white">I'm currently working through <a href="https://www.theodinproject.com/home" rel="noopener noreferrer" target="_blank"><span className="font-bold border-b-2 border-cyan-300 text-odin">The Odin Project</span></a> as well as a personal project when time allows. A recent accomplishment of mine is condensing my first-ever Python script from 600+ lines of code down to 24 lines!</p>
             </div>
         </section>
     </main>

3f5a0bed060cd8ea59322b67bf40c59629bbd98d
rtamburro <ronnie.tamburro@protonmail.com>
2021-07-25 11:45:49 -0400

    history

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { browserHistory, Router, Route, Switch } from "react-router-dom";
+import { browserHistory, Router, Route, Switch } from "react-router";
 import './App.css';
 import Home from "./components/Home";
 import About from "./components/About";
